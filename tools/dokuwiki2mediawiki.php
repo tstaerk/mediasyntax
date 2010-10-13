fb6158f5440d2b99eff4a9c0d51047a88670d81b
root <root@linux-noqb.site>
2010-10-13 19:44:15 +0200

    correcting argument count

--- tools/dokuwiki2mediawiki.php
+++ tools/dokuwiki2mediawiki.php
@@ -9,57 +9,67 @@
 // filename: not only one file name!
 // read: not only 4096 bytes
 // test if we overwrite a file
-// test if argv[1] is empty
 
 if ($argc==1)
 {
-  echo "dokuwiki2mediawiki.php (c) 2010 by Thorsten Staerk";
-  echo "Usage: php dokuwiki2mediawiki <file>";
-  echo "Example: php dokuwiki2mediawiki start.txt";
+  echo "dokuwiki2mediawiki.php (c) 2010 by Thorsten Staerk\n";
+  echo "This program converts dokuwiki syntax to mediawiki syntax.\n";
+  echo "The source file is given as an argument, the target file is the same plus the suffix \".mod\"\n";
+  echo "Usage: php dokuwiki2mediawiki <file>\n";
+  echo "Example: php dokuwiki2mediawiki start.txt\n";
 }
-$filename=$argv[1];
-$inputfile=fopen($filename,"r");
-$outputfile=fopen($filename.".mod","w");
-if ($inputfile)
+
+if ($argc > 2)
 {
-  if ($outputfile)
+  echo "dokuwiki2mediawiki can only process one file at a time.\n";
+}
+
+if ($argc==2)
+{
+  $filename=$argv[1];
+  $inputfile=fopen($filename,"r");
+  $outputfile=fopen($filename.".mod","w");
+  if ($inputfile) 
   {
-    while (!feof($inputfile))
+    if ($outputfile)
     {
-      $line=fgets($inputfile,4096);
+      while (!feof($inputfile))
+      {
+        $line=fgets($inputfile,4096);
 
-      // replace headings
-      if (preg_match('/^ *======.*====== *$/',$line))
-      {
-        $line=preg_replace('/^ *======/','=',$line);
-        $line=preg_replace('/====== *$/','=',$line);
-      }
-      elseif (preg_match('/^ *=====.*===== *$/',$line))
-      {
-        $line=preg_replace('/^ *=====/','==',$line);
-        $line=preg_replace('/===== *$/','==',$line);
-      }
-      elseif (preg_match('/^ *====.*==== *$/',$line))
-      {
-        $line=preg_replace('/^ *====/','===',$line);
-        $line=preg_replace('/==== *$/','===',$line);
-      }
-      elseif (preg_match('/^ *===.*=== *$/',$line))
-      {
-        $line=preg_replace('/^ *===/','====',$line);
-        $line=preg_replace('/=== *$/','====',$line);
-      }
-      elseif (preg_match('/^ *==.*== *$/',$line))
-      {
-        $line=preg_replace('/^ *==/','=====',$line);
-        $line=preg_replace('/== *$/','=====',$line);
-      }
-      // end of replace headings
+        // replace headings
+        if (preg_match('/^ *======.*====== *$/',$line))
+        {
+          $line=preg_replace('/^ *======/','=',$line);
+          $line=preg_replace('/====== *$/','=',$line);
+        }
+        elseif (preg_match('/^ *=====.*===== *$/',$line))
+        {
+          $line=preg_replace('/^ *=====/','==',$line);
+          $line=preg_replace('/===== *$/','==',$line);
+        }
+        elseif (preg_match('/^ *====.*==== *$/',$line))
+        {
+          $line=preg_replace('/^ *====/','===',$line);
+          $line=preg_replace('/==== *$/','===',$line);
+        }
+        elseif (preg_match('/^ *===.*=== *$/',$line))
+        {
+          $line=preg_replace('/^ *===/','====',$line);
+          $line=preg_replace('/=== *$/','====',$line);
+        }
+        elseif (preg_match('/^ *==.*== *$/',$line))
+        {
+          $line=preg_replace('/^ *==/','=====',$line);
+          $line=preg_replace('/== *$/','=====',$line);
+        }
+        // end of replace headings
 
-      fwrite($outputfile,$line);
+        fwrite($outputfile,$line);
+      }
     }
   }
-}
-fclose ($inputfile);
-fclose ($outputfile);
+  fclose ($inputfile);
+  fclose ($outputfile);
+} // if ($argc==2)
 ?>
