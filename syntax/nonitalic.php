449f90a5e2fc19cff1bf399a542f8cec5fad9975
Thorsten Staerk <dev@staerk.de>
2010-10-16 11:49:04 +0200

    do not switch to italic, enabled

--- syntax/nonitalic.php
+++ syntax/nonitalic.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Mediasyntax Plugin, nonitalic component: Mediawiki style //-string 
- * 
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Thorsten Staerk <dev@staerk.de>
- * 
- * This file exists so the mediasyntax plugin does not use the // string as markup for italic
- */
+* Mediasyntax Plugin, nonitalic component: Mediawiki style //-string
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author Thorsten Staerk <dev@staerk.de>
+*
+* This file exists so the mediasyntax plugin does not use the // string as markup for italic
+*/
  
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
@@ -15,30 +15,30 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
  
 /**
- * All DokuWiki plugins to extend the parser/rendering mechanism
- * need to inherit from this class
- */
-class syntax_plugin_mediasyntax_nonitalic extends DokuWiki_Syntax_Plugin 
+* All DokuWiki plugins to extend the parser/rendering mechanism
+* need to inherit from this class
+*/
+class syntax_plugin_mediasyntax_nonitalic extends DokuWiki_Syntax_Plugin
 {
   function getInfo()
   {
     return array(
       'author' => 'Thorsten Stärk, Gina Häußge, Michael Klier, Esther Brunner',
-      'email'  => 'dev@staerk.de',
-      'date'   => '2010-10-16',
-      'name'   => 'Mediasyntax Plugin, non italic component',
-      'desc'   => 'Mediawiki style handling of // string',
-      'url'    => 'http://wiki.splitbrain.org/plugin:mediasyntax',
+      'email' => 'dev@staerk.de',
+      'date' => '2010-10-16',
+      'name' => 'Mediasyntax Plugin, non italic component',
+      'desc' => 'Mediawiki style handling of // string',
+      'url' => 'http://wiki.splitbrain.org/plugin:mediasyntax',
     );
   }
 
-  function getType() { return 'container'; }
-  function getPType() { return 'block'; }
-  function getSort() 
-  { 
+  function getType() { return 'formatting'; }
+
+  function getSort()
+  {
   // emphasis has a sort of 80. Set this to 70 and it will be active.
   // Set it to 90 and it will not be active.
-    return 90; 
+    return 70;
   }
   
   function getAllowedTypes()
@@ -77,6 +77,3 @@ class syntax_plugin_mediasyntax_nonitalic extends DokuWiki_Syntax_Plugin
     return true;
   }
 }
-
-
-//Setup VIM: ex: et ts=4 enc=utf-8 :
