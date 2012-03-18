f58690a54fbb5410aece0a4c9090281470cafb93
Thorsten Staerk <dev@staerk.de>
2012-03-18 08:59:23 +0100

    don't limit line read to 4096 bytes

--- tools/dokuwiki2mediawiki.php
+++ tools/dokuwiki2mediawiki.php
@@ -7,10 +7,9 @@
 
 // TODO:
 // filename: not only one file name!
-// read: not only 4096 bytes
 // test if we overwrite a file
 // test if file exists
-// rowspan in tables should work (::: in dokuwiki syntax)
+// allow rowspan (::: in dokuwiki syntax)
 
 $in_table=false;
 
@@ -28,96 +27,105 @@ for ($argument=1;$argument<$argc;$argument++)
   $filename=$argv[$argument];
   $inputfile=fopen($filename,"r");
   $outputfile=fopen($filename.".mod","w");
+  $i=0;
   if ($inputfile) 
   {
-    if ($outputfile)
+    while (!feof($inputfile))
     {
-      while (!feof($inputfile))
+      $lines[$i++]=fgets($inputfile); //we start counting a 0
+    }
+    fclose($inputfile);
+  }
+  $linecount=$i;
+  $i=-1;
+
+  if ($outputfile)
+  {
+    while (++$i<$linecount)
+    {
+      echo $i;
+      echo $lines[$i];
+      // replace headings
+      if (preg_match('/^ *======.*====== *$/',$lines[$i]))
       {
-        $line=fgets($inputfile,4096);
+        $line=preg_replace('/^ *======/','=',$lines[$i]);
+        $line=preg_replace('/====== *$/','=',$lines[$i]);
+      }
+      elseif (preg_match('/^ *=====.*===== *$/',$lines[$i]))
+      {
+        $line=preg_replace('/^ *=====/','==',$lines[$i]);
+        $line=preg_replace('/===== *$/','==',$lines[$i]);
+      }
+      elseif (preg_match('/^ *====.*==== *$/',$lines[$i]))
+      {
+        $line=preg_replace('/^ *====/','===',$lines[$i]);
+        $line=preg_replace('/==== *$/','===',$lines[$i]);
+      }
+      elseif (preg_match('/^ *===.*=== *$/',$lines[$i]))
+      {
+        $line=preg_replace('/^ *===/','====',$lines[$i]);
+        $line=preg_replace('/=== *$/','====',$lines[$i]);
+      }
+      elseif (preg_match('/^ *==.*== *$/',$lines[$i]))
+      {
+        $line=preg_replace('/^ *==/','=====',$lines[$i]);
+        $line=preg_replace('/== *$/','=====',$lines[$i]);
+      }
+      // end of replace headings
 
-        // replace headings
-        if (preg_match('/^ *======.*====== *$/',$line))
-        {
-          $line=preg_replace('/^ *======/','=',$line);
-          $line=preg_replace('/====== *$/','=',$line);
-        }
-        elseif (preg_match('/^ *=====.*===== *$/',$line))
-        {
-          $line=preg_replace('/^ *=====/','==',$line);
-          $line=preg_replace('/===== *$/','==',$line);
-        }
-        elseif (preg_match('/^ *====.*==== *$/',$line))
-        {
-          $line=preg_replace('/^ *====/','===',$line);
-          $line=preg_replace('/==== *$/','===',$line);
-        }
-        elseif (preg_match('/^ *===.*=== *$/',$line))
-        {
-          $line=preg_replace('/^ *===/','====',$line);
-          $line=preg_replace('/=== *$/','====',$line);
-        }
-        elseif (preg_match('/^ *==.*== *$/',$line))
-        {
-          $line=preg_replace('/^ *==/','=====',$line);
-          $line=preg_replace('/== *$/','=====',$line);
-        }
-        // end of replace headings
+      // replace bulletpoints
+      $level=0; // level of bulletpoints, e.g. * is level 1, *** is level 3.
+      while (preg_match('/^(  )+\*/',$lines[$i]))
+      {
+        $line=preg_replace("/^  /","",$lines[$i]);
+        $level++;
+      }
+      while ($level>1)
+      {
+        $lines[$i]="*".$lines[$i];
+        $level--;
+      }
+      // end of replace bulletpoints
 
-        // replace bulletpoints
-        $level=0; // level of bulletpoints, e.g. * is level 1, *** is level 3.
-        while (preg_match('/^(  )+\*/',$line))
-        {
-          $line=preg_replace("/^  /","",$line);
-          $level++;
-        }
-        while ($level>1)
-        {
-          $line="*".$line;
-          $level--;
-        }
-        // end of replace bulletpoints
+      // replace ordered list items
+      $level=0; // level of list items, e.g. - is level 1, --- is level 3.
+      while (preg_match('/^(  )+\-/',$lines[$i]))
+      {
+        $line=preg_replace("/^  /","",$lines[$i]);
+        $level++;
+      }
+      $line=preg_replace("/^-/","#",$lines[$i]);
+      while ($level>1)
+      {
+        $line="#".$line;
+        $level--;
+      }
+      // end of replace ordered list items
 
-        // replace ordered list items
-        $level=0; // level of list items, e.g. - is level 1, --- is level 3.
-        while (preg_match('/^(  )+\-/',$line))
-        {
-          $line=preg_replace("/^  /","",$line);
-          $level++;
-        }
-        $line=preg_replace("/^-/","#",$line);
-        while ($level>1)
-        {
-          $line="#".$line;
-          $level--;
-        }
-        // end of replace ordered list items
-
-        // replace tables
-        if (preg_match("/^\^/",$line))
-	{
-	  $line=preg_replace("/^\^/","{| class=\"wikitable sortable\" border=1\n!",$line);
-          $line=preg_replace("/\^/","!!",$line);
-	}
-	if (preg_match("/^\|/",$line))
-	{
-	  $line=preg_replace("/\|/","||",$line);
-	  $line=preg_replace("/^\|\|/","|-\n| ",$line);
-	  $in_table=true;
-	}
-	// have we left a table?
-	if ((!preg_match("/^\|/",$line)) && $in_table)
-	{
-	  $line="|-\n|}\n".$line;
-	  $in_table=false;
-	}
+      // replace tables
+      if (preg_match("/^\^/",$line))
+      {
+        $line=preg_replace("/^\^/","{| class=\"wikitable sortable\" border=1\n!",$line);
+        $line=preg_replace("/\^/","!!",$line);
+      }
+      if (preg_match("/^\|/",$line))
+      {
+        $line=preg_replace("/\|/","||",$line);
+        $line=preg_replace("/^\|\|/","|-\n| ",$line);
+        $in_table=true;
+      }
+      // have we left a table?
+      if ((!preg_match("/^\|/",$line)) && $in_table)
+      {
+        $line="|-\n|}\n".$line;
+        $in_table=false;
         fwrite($outputfile,$line);
       }
-      // is the end of file also an end of table?
-      if ($in_table) {fwrite($outputfile,"\n|-\n|}\n");}
-    }
-  }
-  fclose ($inputfile);
-  fclose ($outputfile);
+      fwrite($outputfile,$line);
+    } //while (++$i<$linecount)
+    // is the end of file also an end of table?
+    if ($in_table) {fwrite($outputfile,"\n|-\n|}\n");}
+    fclose ($outputfile);
+  } //if ($outputfile)
 }
 ?>
