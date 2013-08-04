037f9206ec64ef52e6a181b398b0deb24cb646d7
Thorsten Staerk <dev@staerk.de>
2013-08-04 10:54:46 +0200

    adding underline functionality

---
+++ syntax/nonunderline.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* Mediasyntax Plugin, nonunderline component: Mediawiki style //-string
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author Thorsten Staerk <dev@staerk.de>
+*
+* This file exists so the mediasyntax plugin does not use the __ string as markup for italic
+*/
+ 
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+ 
+/**
+* All DokuWiki plugins to extend the parser/rendering mechanism
+* need to inherit from this class
+*/
+class syntax_plugin_mediasyntax_nonunderline extends DokuWiki_Syntax_Plugin
+{
+
+  function getType() 
+  {
+    // source: http://github.com/splitbrain/dokuwiki/blob/master/inc/parser/parser.php#L12
+    return 'formatting';
+  }
+
+  function getSort()
+  {
+    // to overwrite dokuwiki's default, getSort must deliver a lower value
+    return 70;
+  }
+  
+  function getAllowedTypes()
+  {
+    return array('formatting', 'substition', 'disabled', 'protected');
+  }
+  
+  function connectTo($mode)
+  {
+    $this->Lexer->addSpecialPattern('__',$mode,'plugin_mediasyntax_nonunderline');
+  }
+
+  function handle($match, $state, $pos, &$handler)
+  {
+    return array($match, $state, $pos);
+  }
+  
+  function render($mode, &$renderer, $data)
+  {
+    // This is valid globally, not only for xhtml or so.
+    $renderer->doc .= "__";
+    return false;
+  }
+}
---
+++ syntax/underline.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Mediasyntax Plugin: Mediawiki style underline text
+ * <u>..</u> is allowed in mediawiki syntax, see
+ * http://svn.wikimedia.org/viewvc/mediawiki/trunk/phase3/includes/Sanitizer.php?view=markup
+ * This is a part of the mediasyntax plugin for dokuwiki. It handles the <u> tag.
+ * This file must be called underline.php or it will not work.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Thorsten Staerk <dev@staerk.de>
+ */
+ 
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+ 
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_mediasyntax_underline extends DokuWiki_Syntax_Plugin 
+{
+
+  function getType() { return 'protected'; }
+  function getSort() { return 40; }
+ 
+  function connectTo($mode)
+  {
+    $this->Lexer->addEntryPattern(
+      '<u>',
+      $mode,
+      'plugin_mediasyntax_underline'
+    );
+  }
+  
+  function postConnect()
+  {
+    $this->Lexer->addExitPattern(
+      '</u>',
+      'plugin_mediasyntax_underline'
+    );
+  }
+  
+  function handle($match, $state, $pos, &$handler)
+  {
+    if ($state == DOKU_LEXER_UNMATCHED) return array($state,$match);
+    if ($state == DOKU_LEXER_ENTER) return array($state,$match);
+    if ($state == DOKU_LEXER_EXIT) return array($state,$match);
+  }
+  
+  function render($mode, &$renderer, $data)
+  // For understanding this see the very valuable code by Christopher Smith on http://www.dokuwiki.org/devel:syntax_plugins
+  // $data is always what the function handle returned!
+  {
+    list($state,$match) = $data;
+    if ($mode == 'xhtml')
+    {
+      if ($state==DOKU_LEXER_ENTER) $renderer->doc .= "<u>";
+      if ($state==DOKU_LEXER_UNMATCHED) $renderer->doc .= $match;
+      if ($state==DOKU_LEXER_EXIT) $renderer->doc .= "</u>";
+    }
+    return false;
+  }
+}
+     
+//Setup VIM: ex: et ts=4 enc=utf-8 :
