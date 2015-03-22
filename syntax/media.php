d801a4eb5a10be6ab4a8d26e523b171dba3d4d20
Thorsten Staerk <dev@staerk.de>
2015-03-22 12:47:02 +0100

    make images work

--- syntax/media.php
+++ syntax/media.php
@@ -18,22 +18,49 @@ require_once(DOKU_PLUGIN.'syntax.php');
 /** 
  * All DokuWiki plugins to extend the parser/rendering mechanism 
  * need to inherit from this class 
- */ 
+ */
+ 
+// to show an image we need to insert an html syntax like
+// <img src="/dokuwiki/lib/exe/fetch.php?w=200&amp;tok=234ffe&amp;media=test.jpg" class="media" alt="" width="200" />
 class syntax_plugin_mediasyntax_media extends DokuWiki_Syntax_Plugin 
 { 
+  function getType() { return 'substition'; }
 
-    var $helper = null;
+  function getSort() { return 99; }
 
-    function getType() { return 'substition'; }
-    function getSort() { return 99; }
-    function getPType() { return 'block'; }
+  function getPType() { return 'block'; }
 
-    function connectTo($mode) 
-    // connect an [[Image:foo.bar]] occurrence to the handler "media"
-    // goal: e.g. show an image
-    {  
-        $this->Lexer->addSpecialPattern("\[\[Image:.+?\]\]", $mode, 'media');  
-    } 
+  function connectTo($mode) 
+  {  
+    $this->Lexer->addSpecialPattern("\[\[Image:.+?\]\]", $mode, 'plugin_mediasyntax_media');  
+  } 
+  
+  function handle($match, $state, $pos, &$handler)
+  // This first gets called with $state=1 and $match is the entryPattern that matched. 
+  // Then it (the function handle) gets called with $state=3 and $match is the text
+  // between the entryPattern and the exitPattern.
+  // Then it gets called with $state=4 and $match is the exitPattern.
+  // What this delivers is what is handed over as $data to the function render.
+  {
+    return array($match, $state, $pos);
+  }
+  
+  function render($mode, &$renderer, $data)
+  // $data is the return value of handle
+  // $data[0] is always $match
+  // $data[1] is always $state
+  // $data[3] is always $pos
+  {
+    $match=$data[0];
+    $end=preg_replace("/^\[\[Image:/","",$match);
+    $start=preg_replace("/\]\]$/","",$end);
+    
+    if($mode == 'xhtml')
+    {
+      $renderer->doc.= '<img src="'.DOKU_BASE.'lib/exe/fetch.php?media='.$start.'"/>';
+    }
+    return false;
+  }
 
 }
 // vim:ts=4:sw=4:et:enc=utf-8:
