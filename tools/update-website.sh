06dfb4f2522615bb6c937c8683663980604f2b3c
Thorsten Staerk <dev@staerk.de>
2015-07-26 12:26:09 +0200

    cd to script's directory before starting action

--- tools/update-website.sh
+++ tools/update-website.sh
@@ -1,7 +1,10 @@
+# update-website.sh (c) 2015 by Thorsten Staerk
+
 # This will update www.staerk.de/files/mediasyntax.tar.gz
 # you need to have passwordless login established to www.staerk.de
 
+echo $(dirname $0)
+cd $(dirname $0)
 cd ..
 cd ..
-tar cvzf mediasyntax.tar.gz mediasyntax
-scp mediasyntax.tar.gz www.staerk.de:/var/www/staerk.de/files
+tar cvzf mediasyntax.tar.gz mediasyntax && scp mediasyntax.tar.gz www.staerk.de:/var/www/staerk.de/files
